99e74c3f6604bbe3c62d24285011ad3bcc99b449
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-06-21 19:20:04 -0700

    update

--- trim_clippis.py
+++ trim_clippis.py
@@ -1,11 +1,11 @@
 import os
 import json
-sec = 3		# choose how many seconds you want to trim off the end
+frames = 160	# choose how many frames you want to trim off the end (whole numbers only)
 
 for filename in os.listdir():
     if not filename.endswith('json'): continue
     with open(filename) as old, open(filename.replace('.json', '.new.json'), 'w') as new:
         my_dict = json.load(old)
         for clip in my_dict['queue']:
-            clip['endFrame'] -= sec*60 # number of frames
+            clip['endFrame'] -= frames # number of frames
         json.dump(my_dict, new, indent=2)
